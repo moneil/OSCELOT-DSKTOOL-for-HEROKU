fc0ca2e020dbc9c4e458be704f9e26c193a7b4db
Mark O'Neil <zeretic@gmail.com>
2020-07-28 17:01:23 -0400

    additional files and changes for Heroku

--- dsktool/settings.py
+++ dsktool/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9c7cq(30-^yen1j$lt$mdz@_fwudv0^ifv_@ys)abb0f6-0jt('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
