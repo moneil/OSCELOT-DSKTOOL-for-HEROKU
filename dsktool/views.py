89fff7d98d4bd902acdaaf781b4f1766afadf868
Mark O'Neil <zeretic@gmail.com>
2020-07-28 16:36:01 -0400

    additional files and changes for Heroku

---
+++ dsktool/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+from django.shortcuts import render
+from django.urls import reverse
+
+def index(request):
+    
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html')
