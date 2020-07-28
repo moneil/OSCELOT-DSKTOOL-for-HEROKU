ee972b99381b745b578267f1c0c73a965b994d4a
Mark O'Neil <zeretic@gmail.com>
2020-07-28 16:38:50 -0400

    additional files and changes for Heroku

--- dsktool/urls.py
+++ dsktool/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dsktool import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+        path('', views.index, name='index'),
 ]
