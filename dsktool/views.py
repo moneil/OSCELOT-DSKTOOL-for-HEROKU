5ce47cf4bac5e2852506403db45fc42415482dfd
Mark O'Neil <zeretic@gmail.com>
2020-07-28 18:58:08 -0400

    additional files and changes for Heroku

--- dsktool/views.py
+++ dsktool/views.py
@@ -8,8 +8,32 @@ import json
 import os
 import uuid
 
-def index(request):
+try:
+    print("VIEWS.py: using config.py...")
+    from config import adict
+
+    KEY = adict['learn_rest_key']
+    SECRET = adict['learn_rest_secret']
+    LEARNFQDN = adict['learn_rest_fqdn']
+
+except:
+    print("VIEWS.py: using docker-compose env settings...")
     
+    KEY = os.environ['APPLICATION_KEY']
+    SECRET = os.environ['APPLICATION_SECRET']
+    LEARNFQDN = os.environ['BLACKBOARD_LEARN_INSTANCE']
+
+def index(request):
+    bb = BbRest(KEY, SECRET, f"https://{LEARNFQDN}" )
+    resp = bb.GetVersion()
+    access_token = bb.token_info['access_token']
+    version_json = resp.json()
+
+    context = {
+        'learn_server': LEARNFQDN,
+        'version_json' : version_json,
+        'access_token' : access_token,
+    }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html')
+    return render(request, 'index.html', context=context)
