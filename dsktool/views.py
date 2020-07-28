5e08ec28f01ede8b648016b108bbe3b93e38b480
Mark O'Neil <zeretic@gmail.com>
2020-07-28 18:49:11 -0400

    additional files and changes for Heroku

--- dsktool/views.py
+++ dsktool/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import render
 from django.urls import reverse
-
+import bbrest
+from bbrest import BbRest
+import jsonpickle
+import json
+import os
+import uuid
 
 def index(request):
     
