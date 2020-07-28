1c1b00ae6d110fbb515165c94ee7bbb8d042187a
Mark O'Neil <zeretic@gmail.com>
2020-07-28 16:02:17 -0400

    additional files and changes for Heroku

--- dsktool/settings.py
+++ dsktool/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
