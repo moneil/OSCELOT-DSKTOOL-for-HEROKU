f0c941b0905dd39cc942382aaeb0bc8da3790e82
Mark O'Neil <zeretic@gmail.com>
2020-07-28 17:26:46 -0400

    additional files and changes for Heroku

--- dsktool/settings.py
+++ dsktool/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'dsktool.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['dsktool',],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
