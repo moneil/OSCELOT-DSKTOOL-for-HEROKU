910004c5f5cf603a122ab8d5aba03da692203163
Mark O'Neil <zeretic@gmail.com>
2020-07-28 23:18:05 -0400

    sessions

--- dsktool/settings.py
+++ dsktool/settings.py
@@ -135,3 +135,6 @@ STATICFILES_DIRS = (
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
